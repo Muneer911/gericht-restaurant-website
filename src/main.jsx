d98e557edcb18418b3eef1b057c511d008dad751
Muneer <ahmed.elsadig.ahmed@gmail.com>
2024-01-25 21:07:36 +0200

    docs: add more routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { AboutUs, SpecialMenu, Laurels, Footer } from "./container/index.js";
 import "./index.css";
-import RG from './components/Register/RG.jsx'
-import Navbar from './components/Navbar/Navbar.jsx'
+import RG from "./components/Register/RG.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<App />}/>
-        <Route path="/Register" element={<RG/>}>
-
-        </Route>
-    </Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Register" element={<RG />}></Route>
+        <Route path="/about" element={<AboutUs />}></Route>
+        <Route path="/menu" element={<SpecialMenu />}></Route>
+        <Route path="/laurels" element={<Laurels />}></Route>
+      </Routes>
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
