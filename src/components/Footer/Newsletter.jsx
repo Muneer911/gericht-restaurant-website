d22480191de1a83c36cad40404df154ceb65748e
Muneer <ahmed.elsadig.ahmed@gmail.com>
2024-01-25 20:38:29 +0200

    feat: require alert onSubmit

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import SubHeading from '../SubHeading/SubHeading';
-import './Newsletter.css';
+import SubHeading from "../SubHeading/SubHeading";
+import "./Newsletter.css";
 
 const Newsletter = () => (
   <div className="app__newsletter">
@@ -10,10 +10,12 @@ const Newsletter = () => (
       <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
       <p className="p__opensans">And never miss latest Updates!</p>
     </div>
-    <div className="app__newsletter-input flex__center">
-      <input type="email" placeholder="Enter your email address" />
-      <button type="button" className="custom__button">Subscribe</button>
-    </div>
+    <form className="app__newsletter-input flex__center">
+      <input required type="email" placeholder="Enter your email address" />
+      <button type="submit" className="custom__button">
+        Subscribe
+      </button>
+    </form>
   </div>
 );
 
