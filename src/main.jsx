df5b5db359a9619fa1e3dc83db1eb9d13c43c764
Muneer <ahmed.elsadig.ahmed@gmail.com>
2024-01-28 10:45:09 +0200

    docs: organize the import

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { AboutUs, SpecialMenu, Laurels, Footer } from "./container/index.js";
 import "./index.css";
-import RG from "./components/Register/RG.jsx";
-import Navbar from "./components/Navbar/Navbar.jsx";
+import {
+  AboutUs,
+  SpecialMenu,
+  Laurels,
+  Footer,
+  RG,
+  Navbar,
+  App,
+  ForgetPassword,
+} from "./container/index.js";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -14,6 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Register" element={<RG />}></Route>
+        <Route path="/ForgetPassword" element={<ForgetPassword />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
         <Route path="/menu" element={<SpecialMenu />}></Route>
         <Route path="/laurels" element={<Laurels />}></Route>
